c55bd8e661b31911f1a54ee4926aeb2ed604ec82
Uchenna Nebedum <nebeduch@gmail.com>
2018-09-11 21:25:33 +0100

    few changes to script

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ A script to scrape youtube links from a predefined website of choice.
 
 #funtion to extract youtube link from web pages
 urls=deque()
-urls.append('https://www.examsolutions.net/tutorials/resultant-forces-two-forces-angle/?level=A-Level&board=AQA&module=Mechanics%20A-Level&topic=1632')
+urls.append('https://url_to_crawl')
 def process_urls(urls_to_process):
 
 	processed_urls = set()  # a set of urls that have already been crawled
@@ -34,28 +34,23 @@ def process_urls(urls_to_process):
 			continue
 		new_youtube_link = set(re.findall(r"https://www.youtube.com/embed/[a-z,A-Z,0-9]+", response.text, re.I))   # extract all youtube_link addresses and add them into a set
 		youtube_links.update(new_youtube_link)
-		soup = BeautifulSoup(response.text)     # create a beutiful soup for the html document
+		soup = BeautifulSoup(response.text)     # create a beautifulsoup for the html document
 
 		if new_youtube_link:
 			f.write('\n'.join(new_youtube_link))
 			f.write('\n')
 		f.close()
 
-
+		#Extract more urls from current page after crawl :'<a> tags'
 		for anchor in soup.find_all("a"):
 			link = anchor.attrs["href"] if "href" in anchor.attrs else ''  # extract link url from the anchor
 			use_link = ''
-			if link.startswith('https://www.examsolutions.net/'):   # resolve relative links
+			if link.startswith('base_url'):   # ensure link is part of current domain not cross-site 
 				use_link = link
 
 			if not use_link in urls_to_process and not use_link in processed_urls:   # add the new url to the queue if it was not enqueued nor processed yet
 				urls_to_process.append(use_link)
 
-		
-	# f = open('Rainmails.txt','w')    #indicate name of output file to write youtube_links
-	#sys.stdout = f
-	#path= '/home/Desktop'  #for linux
-	# f.write(repr(youtube_links))
 
 	return
 
